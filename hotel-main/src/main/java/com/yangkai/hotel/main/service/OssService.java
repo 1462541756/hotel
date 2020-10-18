a70bd95ab6d1edc6e0dc5047c21a72fd998bf22b
test <1462541756@qq.com>
2020-10-18 19:03:57 +0800

    "添加oss相关服务"

---
+++ hotel-main/src/main/java/com/yangkai/hotel/main/service/OssService.java
@@ -0,0 +1,21 @@
+package com.yangkai.hotel.main.service;
+
+import com.yangkai.hotel.main.dto.OssCallbackResult;
+import com.yangkai.hotel.main.dto.OssPolicyResult;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * oss上传管理Service
+ * Created by macro on 2018/5/17.
+ */
+public interface OssService {
+    /**
+     * oss上传策略生成
+     */
+    OssPolicyResult policy();
+    /**
+     * oss上传成功回调
+     */
+    OssCallbackResult callback(HttpServletRequest request);
+}
